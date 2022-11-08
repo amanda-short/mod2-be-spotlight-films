bf4935db916200b38809d0de9c008d1d7ad106a9
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-11-08 14:10:32 -0800

    test 11 passed

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,14 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT film.title,
+COUNT(rental.inventory_id)
+AS COUNT
+FROM rental
+LEFT JOIN inventory
+ON rental.inventory_id = inventory.inventory_id
+LEFT JOIN film
+ON film.film_id = inventory.film_id
+GROUP BY film.film_id
+ORDER BY count DESC
+LIMIT 10
